6f26e58b26f99c4a2e2798177f06e541d0184695
Prasanna Adhikari <prasannaadhikari2602@gmail.com>
2026-09-13 14:49:40 +0545

    Add cloudinary_storage to INSTALLED_APPS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "cloudinary_storage",
     "django.contrib.staticfiles",
 
     # Third-party
