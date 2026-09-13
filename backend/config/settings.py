1d1621fbd29d9875b86bf78370350afc13f15d80
Prasanna Adhikari <prasannaadhikari2602@gmail.com>
2026-09-13 13:50:42 +0545

    update settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -175,16 +175,15 @@ CSRF_TRUSTED_ORIGINS = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-
         "NAME": os.getenv("DB_NAME"),
-
         "USER": os.getenv("DB_USER"),
-
         "PASSWORD": os.getenv("DB_PASSWORD"),
-
         "HOST": os.getenv("DB_HOST", "localhost"),
-
         "PORT": os.getenv("DB_PORT", "5432"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+        "DISABLE_SERVER_SIDE_CURSORS": True,
     }
 }
 
